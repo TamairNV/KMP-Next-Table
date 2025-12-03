4c3de3ebc6e7580a07819c825dd60cc0757fa798
Tamer Vassib <147373183+TamairNV@users.noreply.github.com>
2025-12-03 12:59:22 +0000

    Refactor nexttable to use generateTable for output
    Refactor nexttable function to remove addLine and implement generateTable for displaying results.

--- nexttable.js
+++ nexttable.js
@@ -1,77 +1,64 @@
 /** Main Algorithm **/
-function nexttable(input){
+function nexttable(input) {
   // Clears the result area
-  document.getElementById("result").innerHTML = " ";
-  
+  document.getElementById("result").innerHTML = "";
+
   // Split Input into an Array 
   var A = input.split("");
-  addLine(A);
-  
-  // Next table
+  // Removed addLine(A) here so we can show it in the table instead
+
+  // Next table logic
   var B = new Array(A.length);
-  
-  B[0] = -1;                    // First value always -1
-  B[1] = (A[1]==A[0])? -1 : 0;  // Second value based on equality of first and second element
-  
-  for(var failPos = 2; failPos < A.length; failPos++){ // start from 2 cause 0 and 1 are done
-    var flag = false; // to identify if value for current failPos is assigned or not
-    
-    for(var i = 1; i < failPos; i++){
-      /* comparing sub arrays e.g.
-       *            x <--- failed at position 4
-       * P = A C A C B
-       *       C A C <--- subA
-       *       A C A <--- subB
-       *  Skipped using sub array from pos 0 because it's obvious to fail'
-       */
+
+  if (A.length > 0) B[0] = -1; // Safety check for empty input
+  if (A.length > 1) B[1] = (A[1] == A[0]) ? -1 : 0;
+
+  for (var failPos = 2; failPos < A.length; failPos++) {
+    var flag = false;
+
+    for (var i = 1; i < failPos; i++) {
       var subA = A.slice(i, failPos);
       var subB = A.slice(0, subA.length);
-      
-      // Returns true is both are same
+
       var same = compareStrArray(subA, subB);
-      
-      if(same){
-        // Check if the Char below A[failPos] and A[failPos] are not the same
-        // If not same, then succeed and move onto next failPos
-        if(A[failPos] !== A[subA.length]){
+
+      if (same) {
+        if (A[failPos] !== A[subA.length]) {
           B[failPos] = subA.length;
           flag = true;
           break;
-        }
-        else if(subB.length === 1 && subB[0] !== A[failPos]){
+        } else if (subB.length === 1 && subB[0] !== A[failPos]) {
           B[failPos] = 0;
           flag = true;
           break;
-        }
-        else {
+        } else {
           continue;
         }
-      }
-      else{
-        if(subB.length === 1 && subB[0] !== A[failPos]){
+      } else {
+        if (subB.length === 1 && subB[0] !== A[failPos]) {
           B[failPos] = 0;
           flag = true;
           break;
-        }
-        else{
+        } else {
           continue;
         }
       }
     }
-    
-    if(!flag) B[failPos] = -1;
+
+    if (!flag) B[failPos] = -1;
   }
-  displayArray(B);
+
+  // Instead of displayArray, we call the table generator
+  generateTable(A, B);
 }
 
 /** Helper Functions **/
-function compareStrArray(arrA, arrB){
-  if(arrA.length != arrB.length){
+function compareStrArray(arrA, arrB) {
+  if (arrA.length != arrB.length) {
     return false;
-  }
-  else {
-    for(var i = 0; i < arrA.length; i++){
-      if(arrA[i] !== arrB[i]){
+  } else {
+    for (var i = 0; i < arrA.length; i++) {
+      if (arrA[i] !== arrB[i]) {
         return false;
       }
     }
@@ -79,15 +66,52 @@ function compareStrArray(arrA, arrB){
   }
 }
 
-function displayArray(arr){
-  for(var i = 0; i < arr.length; i++){
-    addLine(i + " == " + arr[i]);
-  }
-}
+// New function to create the HTML table
+function generateTable(patternArr, nextArr) {
+  var resultDiv = document.getElementById("result");
+  var table = document.createElement("table");
 
-function addLine(string){
-  var div = document.createElement("DIV");
-  var t = document.createTextNode(string);
-  div.appendChild(t);
-  document.getElementById("result").appendChild(div);
+  // Row 1: Indices
+  var trIndex = document.createElement("tr");
+  var thIndex = document.createElement("th");
+  thIndex.innerText = "Index";
+  trIndex.appendChild(thIndex);
+  
+  for (var i = 0; i < patternArr.length; i++) {
+    var td = document.createElement("td");
+    td.innerText = i;
+    td.style.color = "#888"; // Make index numbers slightly lighter
+    trIndex.appendChild(td);
+  }
+  table.appendChild(trIndex);
+
+  // Row 2: Pattern Char
+  var trChar = document.createElement("tr");
+  var thChar = document.createElement("th");
+  thChar.innerText = "Pattern";
+  trChar.appendChild(thChar);
+
+  for (var i = 0; i < patternArr.length; i++) {
+    var td = document.createElement("td");
+    td.innerText = patternArr[i];
+    td.style.fontWeight = "bold"; // Emphasize the pattern
+    trChar.appendChild(td);
+  }
+  table.appendChild(trChar);
+
+  // Row 3: Next Values
+  var trNext = document.createElement("tr");
+  var thNext = document.createElement("th");
+  thNext.innerText = "Next";
+  trNext.appendChild(thNext);
+
+  for (var i = 0; i < nextArr.length; i++) {
+    var td = document.createElement("td");
+    td.innerText = nextArr[i];
+    td.style.color = "#007bff"; // Blue color for the results
+    trNext.appendChild(td);
+  }
+  table.appendChild(trNext);
+
+  resultDiv.appendChild(table);
 }
